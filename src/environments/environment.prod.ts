bb3b7e9c5b49d8a6733fb472e65f65ad3bb8c352
rehanabbasi <rehan.abbasi@acit.com>
2020-04-11 19:00:59 +0500

    Setting prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBrYQbmm-A9DswBupcl6q8IWlMjrW85oNw",
+    authDomain: "rfx-tracker.firebaseapp.com",
+    databaseURL: "https://rfx-tracker.firebaseio.com",
+    projectId: "rfx-tracker",
+    storageBucket: "rfx-tracker.appspot.com",
+    messagingSenderId: "832933326424",
+    appId: "1:832933326424:web:8aeb417d40deafcc02e42d",
+    measurementId: "G-K8E173XKKW"
+  }
 };
